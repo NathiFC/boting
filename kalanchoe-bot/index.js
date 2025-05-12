794bc1013c7577c03ba4b0a9880991b4f5c89338
NathiFC <nathaliachaves63@gmail.com>
2025-05-12 20:00:08 -0300

    Add files via upload

---
+++ kalanchoe-bot/index.js
@@ -0,0 +1,49 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Bot está pronto!');
+});
+
+client.on('message', message => {
+    const msg = message.body.toLowerCase();
+    if (msg.includes('oi') || msg.includes('olá') || msg.includes('ola')) {
+        message.reply(`Olá, tudo bem?
+Seja bem-vindo à Kalanchoe Flores!
+
+Responderemos assim que possível. Para solicitação de orçamentos, por gentileza nos informe:
+
+- Data do evento (dia, mês e ano)
+- Local do evento
+
+E o tipo de decoração:
+- cerimônia religiosa
+- buquê de noiva
+- arranjos em geral
+
+Atenciosamente,
+Kalanchoe Flores`);
+    }
+});
+
+client.initialize();
